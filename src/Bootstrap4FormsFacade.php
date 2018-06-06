28dbb71b13f3ddd8e7afbad3ec6dac99e8556a39
José Neto <sputinykster@gmail.com>
2018-06-06 07:40:02 +0100

    code formating

--- src/Bootstrap4FormsFacade.php
+++ src/Bootstrap4FormsFacade.php
@@ -5,7 +5,10 @@ namespace NetoJose\Bootstrap4Forms;
 use \Illuminate\Support\Facades\Facade;
 
 class Bootstrap4FormsFacade extends Facade {
-    protected static function getFacadeAccessor() {
+
+    protected static function getFacadeAccessor()
+    {
         return 'bootstrap4-form';
     }
+
 }
--- src/Bootstrap4FormsServiceProvider.php
+++ src/Bootstrap4FormsServiceProvider.php
@@ -5,16 +5,19 @@ namespace NetoJose\Bootstrap4Forms;
 use Illuminate\Support\ServiceProvider;
 
 class Bootstrap4FormsServiceProvider extends ServiceProvider {
-    
+
     protected $defer = true;
-    
-    public function register() {
-        $this->app->singleton('bootstrap4-form', function(){
+
+    public function register()
+    {
+        $this->app->singleton('bootstrap4-form', function() {
             return new FormService();
         });
     }
-    
-    public function provides() {
+
+    public function provides()
+    {
         return ['bootstrap4-form'];
     }
+
 }
